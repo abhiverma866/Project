e5c0ef5afbef52f143981935ca4f62a242dffeee
abhiverma866 <abhiverma866@gmail.com>
2019-05-24 01:15:51 +0530

    Updated script.js for writing to log files

--- script.js
+++ script.js
@@ -1,52 +1,64 @@
 //try { load("nashorn:mozilla_compat.js"); } catch(e) {}
 //importClass(org.contikios.cooja.plugins.ScriptRunner);
 
-TIMEOUT(900000);
+try { load("nashorn:mozilla_compat.js"); } catch(e) {}
+importPackage(java.io);
 
-// function printFinalResults(){
-//     var i; 
-//     for(i = 1; i <= nodeCount; i++){
-//         totalReceived += packetsReceived[i];
-//         totalSent += packetsSent[i];
-//     }
+// get plugin instances
+powertracker = mote.getSimulation().getCooja().getStartedPlugin("PowerTracker");
 
-//     log.log("-----Final Results------" + "\n");
-//     //log.log("totalLatency= " + totalLatency + "\n");
-//     //log.log("totalCount= " + totalCount + "\n");
-//     log.log("Generated Packets " + totalSent + "\n");
-//     log.log("ReceivedPackets " + totalReceived + "\n");
-//     PDR = (totalReceived / totalSent);
-//     AE2ED = ((totalLatency / totalReceived)/1000000);
-//     //log.log("Packet Delivery Ratio " + PDR + "\n");
-//     //log.log("Average End to End Delay " + AE2ED + "\n");
+// create log file handlers
+log.log("Opening log file writers...\n");
+log_serial = new FileWriter("/home/abhi/contiki/examples/ipv6/suppression-attack/data/serial.log");                // open serial log file
+log_rpl = new FileWriter("/home/abhi/contiki/examples/ipv6/suppression-attack/data/rpl.log");                      // open RPL messages log file
+log_power = new FileWriter("/home/abhi/contiki/examples/ipv6/suppression-attack/data/powertracker.log");
+log_script = new FileWriter("/home/abhi/contiki/examples/ipv6/suppression-attack/data/script.log");           // open script logfile
 
+TIMEOUT(300000);
 
-//     log.log("TL = " + totalLatency + " | " + " AE2ED = " + AE2ED + " | " +" PDR = " + PDR + "\n");
-// }
+var c = 0, i = 1, period = 30000000;
 
 timeout_function = function () {
-    log.log("Script timed out.\n");
-    log.log("Performance Calculation \n")
-    var i; 
-    totalReceived = totalSent = 0;
-    for(i = 1; i <= nodeCount; i++){
-        totalReceived += packetsReceived[i];
-        totalSent += packetsSent[i];
+    try{
+        //log.log("Script timed out.\n");
+        line = "Script timed out" + "\n"
+        log_script.write(line);
+        log_script.flush();
+        //log.log("Performance Calculation \n");
+        line = "Performance Calculation" + "\n"
+        log_script.write(line);
+        log_script.flush();
+
+        var i; 
+        totalReceived = totalSent = 0;
+        for(i = 1; i <= nodeCount; i++){
+            totalReceived += packetsReceived[i];
+            totalSent += packetsSent[i];
+        }
+
+        //log.log("-----Final Results------" + "\n");
+        //log.log("totalLatency= " + totalLatency + "\n");
+        //log.log("totalCount= " + totalCount + "\n");
+        //log.log("Generated Packets " + totalSent + "\n");
+        //log.log("ReceivedPackets " + totalReceived + "\n");
+        line = "Generated Packets " + totalSent + " | " +  "ReceivedPackets " + totalReceived + "\n"
+        log_script.write(line);
+        log_script.flush();
+        PDR = (totalReceived / totalSent);
+        AE2ED = ((totalLatency / totalReceived)/1000000);
+        //log.log("Packet Delivery Ratio " + PDR + "\n");
+        //log.log("Average End to End Delay " + AE2ED + "\n");
+   
+        //log.log("TL = " + totalLatency + " | " + " AE2ED = " + AE2ED + " | " +" PDR = " + PDR + "\n");
+        line  = "TL = " + totalLatency + " | " + " AE2ED = " + AE2ED + " | " +" PDR = " + PDR + "\n"
+        log_script.write(line);
+        log_script.flush();
+        log.testOK();
+    } catch (e) {
+        log_script.close();
+            log.log("Final results written to script.log closed \n");
+
     }
-
-    log.log("-----Final Results------" + "\n");
-    //log.log("totalLatency= " + totalLatency + "\n");
-    //log.log("totalCount= " + totalCount + "\n");
-    log.log("Generated Packets " + totalSent + "\n");
-    log.log("ReceivedPackets " + totalReceived + "\n");
-    PDR = (totalReceived / totalSent);
-    AE2ED = ((totalLatency / totalReceived)/1000000);
-    //log.log("Packet Delivery Ratio " + PDR + "\n");
-    //log.log("Average End to End Delay " + AE2ED + "\n");
-
-
-    log.log("TL = " + totalLatency + " | " + " AE2ED = " + AE2ED + " | " +" PDR = " + PDR + "\n");
-    log.testOK();
 }
 
 packetsReceived= new Array();
@@ -134,75 +146,113 @@ when we send the DATA we save node, seqno/packetnr, time to a table 'send'
 # and delete that entry from send table, at the end printing send table gives lost packets
 */
 
+
+log.log("Starting stript...\n");
 while(1){
-    YIELD();
-    msgArray = msg.split(' ');
-    //log.log(msgArray+ "\n");
-    //log.log(msgArray[0]+ " " + msgArray[1]);
-    //log.log(msgArray.length + "\n");
-    if(msgArray.length == 9){
-        if(msgArray[0].equals("Sending")){
-            
-            // sent packet
-            senderID = parseInt(msgArray[8]);
-            packetsSent[senderID]++;
-            //var id = msgArray[8];
-            seq = parseInt(msgArray[5]);
-            //log.log("seq " + seq + "\n")
-            st = Number(time);
-            entry = {node_id:senderID, seq_no:seq, send_time:st};
-            //log.log("Sending entry to saveSendTime " + entry.node_id + ", " +  entry.seq_no + ","  + entry.send_time + "\n");
-
-            //log.log("enrty.node_id " + entry.node_id + "\n");
-            saveSendTime(entry);
-            // log.log("On Sending\n");
-            // for(j=0; j<send_table.length;j++){
-            //     log.log("send_table" + "[" + j + "]" + send_table[j].node_id + " | " +  send_table[j].seq_no + " | "  + send_table[j].send_time + "\n"); 
-            //  }            
+    try {
+        // first, log to serial file
+        line = time + "\tID:" + id.toString() + "\t" + msg + "\n"
+        if (msg.startsWith("RPL: ")) {
+          log_rpl.write(line);
+          log_rpl.flush();
+        } else {
+            log_serial.write(line);
+            log_serial.flush();
         }
-    }
-    if(msgArray.length == 14){
-        if(msgArray[0].startsWith("Received")){
-            //log.log("On Receiving\n");
-            
-            //log.log(msgArray.length + "\n");
-            senderID = parseInt(msgArray[10]);
-            seq = parseInt(msgArray[4]);
-            recv_time = Number(time);
-            receiverID = parseInt(msgArray[13]);
-            packetsReceived[receiverID]++;
-            totalReceived = totalSent = 0;
-            
-            entry = {node_id:senderID, seq_no:seq};
-            //log.log("Sending entry send to lookupSendTime " + entry.node_id + ", " +  entry.seq_no + "\n");
-            sendTime = lookupSendTime(entry);
-            // for(j=0; j<send_table.length;j++){
-            //     log.log("send_table" + "[" + j + "]" + send_table[j].node_id + " | " +  send_table[j].seq_no + " | "  + send_table[j].send_time + "\n"); 
-            //  }
-            if (sendTime > -1){
-                //we have a match in sendTable
-                totalLatency = totalLatency + (recv_time - sendTime);
-            }           
-         
-            for(i = 1; i <= nodeCount; i++){
-                totalReceived += packetsReceived[i];
-                totalSent += packetsSent[i];
-                //og.log("MoteID= " + i + " ReceivedPackets= " + packetsReceived[i] + " SendingPackets= " + packetsSent[i] + "\n");
+        YIELD();
+        // then, log power statistics
+        if (c < time) {
+            log_power.write(powertracker.radioStatistics());
+            log_power.flush();
+            c += period;
+          }
+        msgArray = msg.split(' ');
+        //log.log(msgArray+ "\n");
+        //log.log(msgArray[0]+ " " + msgArray[1]);
+        //log.log(msgArray.length + "\n");
+        if(msgArray.length == 9){
+            if(msgArray[0].equals("Sending")){
+                // sent packet
+                senderID = parseInt(msgArray[8]);
+                packetsSent[senderID]++;
+                //var id = msgArray[8];
+                seq = parseInt(msgArray[5]);
+                //log.log("seq " + seq + "\n")
+                st = Number(time);
+                entry = {node_id:senderID, seq_no:seq, send_time:st};
+                //log.log("Sending entry to saveSendTime " + entry.node_id + ", " +  entry.seq_no + ","  + entry.send_time + "\n");
+
+                //log.log("enrty.node_id " + entry.node_id + "\n");
+                saveSendTime(entry);
+                // log.log("On Sending\n");
+                // for(j=0; j<send_table.length;j++){
+                //     log.log("send_table" + "[" + j + "]" + send_table[j].node_id + " | " +  send_table[j].seq_no + " | "  + send_table[j].send_time + "\n"); 
+                //  }            
             }
-            //log.log("totalLatency= " + totalLatency + "\n");
-            //log.log("totalCount= " + totalCount + "\n");
-            log.log("Generated Packets " + totalSent + "\n");
-            log.log("ReceivedPackets " + totalReceived + "\n");
-            PDR = (totalReceived / totalSent);
-            AE2ED = ((totalLatency / totalReceived)/1000000);
-            //log.log("Packet Delivery Ratio " + PDR + "\n");
-            //log.log("Average End to End Delay " + AE2ED + "\n");
-
-
-            log.log("TL = " + totalLatency + " | " + " AE2ED = " + AE2ED + " | " +" PDR = " + PDR + "\n");
         }
-    }  
+        if(msgArray.length == 14){
+            if(msgArray[0].startsWith("Received")){
+                //log.log("On Receiving\n");
+            
+                //log.log(msgArray.length + "\n");
+                senderID = parseInt(msgArray[10]);
+                seq = parseInt(msgArray[4]);
+                recv_time = Number(time);
+                receiverID = parseInt(msgArray[13]);
+                packetsReceived[receiverID]++;
+                totalReceived = totalSent = 0;
+            
+                entry = {node_id:senderID, seq_no:seq};
+                //log.log("Sending entry send to lookupSendTime " + entry.node_id + ", " +  entry.seq_no + "\n");
+                sendTime = lookupSendTime(entry);
+                // for(j=0; j<send_table.length;j++){
+                //     log.log("send_table" + "[" + j + "]" + send_table[j].node_id + " | " +  send_table[j].seq_no + " | "  + send_table[j].send_time + "\n"); 
+                //  }
+                if (sendTime > -1){
+                    //we have a match in sendTable
+                    totalLatency = totalLatency + (recv_time - sendTime);
+                }           
+         
+                for(i = 1; i <= nodeCount; i++){
+                    totalReceived += packetsReceived[i];
+                    totalSent += packetsSent[i];
+                    //og.log("MoteID= " + i + " ReceivedPackets= " + packetsReceived[i] + " SendingPackets= " + packetsSent[i] + "\n");
+                }
+                //log.log("totalLatency= " + totalLatency + "\n");
+                //log.log("totalCount= " + totalCount + "\n");
+                //log.log("Generated Packets " + totalSent + "\n");
+                line = "Generated Packets " + totalSent + " | " +  "ReceivedPackets " + totalReceived + "\n"
+                log_script.write(line);
+                log_script.flush();
+                //log.log("ReceivedPackets " + totalReceived + "\n");
+                PDR = (totalReceived / totalSent);
+                AE2ED = ((totalLatency / totalReceived)/1000000);
+                //log.log("Packet Delivery Ratio " + PDR + "\n");
+                //log.log("Average End to End Delay " + AE2ED + "\n");
+
+
+                //log.log("TL = " + totalLatency + " | " + " AE2ED = " + AE2ED + " | " +" PDR = " + PDR + "\n");
+                line  = "TL = " + totalLatency + " | " + " AE2ED = " + AE2ED + " | " +" PDR = " + PDR + "\n"
+                log_script.write(line);
+                log_script.flush();
+            }
+        }
+    }   catch(e){  
+            log_serial.close();
+            log_rpl.close();
+            log_power.close();
+            log_script.close();
+            log.log("File writers closed\n");
+            if (c == 0) { 
+                log.testFailed(); 
+            } else{    
+                break; 
+            }
+            break;
+        }  
 }
+
+
 
 
 
